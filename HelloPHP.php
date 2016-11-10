1dc32fd0d005511393e0830723d8277064be55fb
Agata <agata@studiowings.pl>
2016-11-10 12:28:28 +0100

    warsztaty

---
+++ HelloPHP.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+if($_SERVER['REQUEST_METHOD']=='POST'&&!empty($_POST['name'])){
+echo 'Cześć ' .$_POST['name'];
+}
+
+
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
